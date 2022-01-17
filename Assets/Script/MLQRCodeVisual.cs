5c62f95dfe39f7389531fc17cbf6b30d09ac9656
Hirotaka Baba <babarin0512business@gmail.com>
2022-01-18 02:49:23 +0900

    QRコード実装済

---
+++ Assets/Script/MLQRCodeVisual.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class MLQRCodeVisual : MonoBehaviour
+{
+   [SerializeField]
+   private TextMesh dataText;
+#if UNITY_LUMIN
+   private Timer disableTimer;
+#endif
+
+   void Awake()
+   {
+#if UNITY_LUMIN
+       disableTimer = new Timer(3f);
+#endif
+   }
+
+   void Update()
+   {
+#if UNITY_LUMIN
+       if (gameObject.activeSelf && disableTimer.LimitPassed)
+       {
+           gameObject.SetActive(false);
+       }
+#endif
+   }
+
+   public void Set(MLBarcodeScanner.BarcodeData data)
+   {
+#if UNITY_LUMIN
+       disableTimer?.Reset();
+#endif
+
+       transform.position = data.Pose.position;
+       transform.rotation = data.Pose.rotation;
+       dataText.text = data.ToString();
+       gameObject.SetActive(true);
+   }
+}
